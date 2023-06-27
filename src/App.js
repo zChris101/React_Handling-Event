1bd2321070f8237e583ae16261841ded4a1324cc
zCris101 <chrstnmusico@gmail.com>
2023-06-27 11:00:26 +0800

    handling event complete

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import "./styles.css";
+import React, { useState } from "react";
+
+
+
+export default function App() {
+
+  const [headingText, setHeadingText] = useState("Hello");
+  const [isMousedOver, setMouseOver] = useState(false);
+
+
+  function handleClick() {
+    setHeadingText("Submitted");
+  }
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
+  return (
+    <div className="container">
+      <h1>{headingText}</h1>
+      <input type="text" placeholder="What's your name?" />
+      <button
+        onClick={handleClick}
+        style={{ backgroundColor: isMousedOver ? "black" : "white" }}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        Submit
+      </button>
+    </div>
+  );
+}
