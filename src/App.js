b2bca56f1c7ea6b2835e693ba1b90dfab64e77f9
zCris101 <chrstnmusico@gmail.com>
2023-06-27 11:47:37 +0800

    added form and submit button functionality

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import React, { useState } from "react";
 
 export default function App() {
 
-  const [headingText, setHeadingText] = useState("Hello");
+  const [name, setName] = useState("")
+  const [headingText, setHeadingText] = useState("");
   const [isMousedOver, setMouseOver] = useState(false);
 
 
-  function handleClick() {
-    setHeadingText("Submitted");
+  function handleClick(event) {
+    setHeadingText(name);
+    console.log({ name });
+
+    event.preventDefault();
+
   }
 
   function handleMouseOver() {
@@ -21,18 +26,30 @@ export default function App() {
     setMouseOver(false);
   }
 
+  function handleChange(event) {
+    console.log(event.target.value);
+    setName(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>{headingText}</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button
-        onClick={handleClick}
-        style={{ backgroundColor: isMousedOver ? "black" : "white" }}
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
-      >
-        Submit
-      </button>
+      <h1>Hello {name} {headingText}</h1>
+      <form onSubmit={handleClick}>
+        <input
+          type="text"
+          placeholder="What's your name?"
+          onChange={handleChange}
+          value={name}
+        />
+        <button
+          style={{ backgroundColor: isMousedOver ? "black" : "white" }}
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
